98c584796ff836a93a0d38c4270786ea5ec11662
def-turtle <turtle48sigma@gmail.com>
2026-02-06 16:50:55 +0100

    fixed(server+send_req2)

--- download_file_site.py
+++ download_file_site.py
@@ -1,4 +1,3 @@
-import io
 from tempfile import NamedTemporaryFile
 from pathlib import Path
 import flask
@@ -15,7 +14,7 @@ def home():
 @app.route("/example-file_download")
 def download_file():
     file_name = "example.txt"
-    random_content = "This is an example file for download."
+    random_content = "bla bla bla " * 1000  # Simulate a large file by repeating content
     # Create a temporary file and return it
     temp_file_path = FILESDIR / "temp_download.txt"
     with open(temp_file_path, 'w') as f:
@@ -35,7 +34,7 @@ def upload_file():
     if file.filename == '':
         return "No selected file", 400
     save_path = FILESDIR / file.filename
-    file.save(save_path)
+    file.save(str(save_path))
     return f"File '{file.filename}' uploaded successfully to {save_path}!", 200
 # @app.route("/example_file_download_2")
 # def download_file_2():
